f60b86565dcffe003fdf250d6d91094364fb71ab
Megha Maheshwari <meghaharlalka@gmail.com>
2017-12-19 16:04:51 -0800

    Modified dl_detector to handle simulation graphs and added the pbtxt file for sim

--- ros/src/tl_detector/light_classification/dl_detector.py
+++ ros/src/tl_detector/light_classification/dl_detector.py
@@ -2,5 +2,5 @@
 
 class DeepLearningDetector:
   def __init__(self):
-    PATH_TO_CKPT = 'models/frozen_inference_graph.pb'
-    PATH_TO_LABELS = 'models/boschlabel.pbtxt'
+    PATH_TO_CKPT = 'models/sim_frozen_inference_graph.pb'
+    PATH_TO_LABELS = 'models/object-detection.pbtxt'
