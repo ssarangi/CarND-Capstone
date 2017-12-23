b9a88f12ec1de874ab7a99c97365e856295b93f5
Megha Maheshwari <meghaharlalka@gmail.com>
2017-12-22 17:12:21 -0800

    Modified dl detector to return the detected trafiic light

--- ros/src/tl_detector/light_classification/dl_detector.py
+++ ros/src/tl_detector/light_classification/dl_detector.py
@@ -1,6 +1,67 @@
+import tensorflow as tf
+import numpy as np
+
+# Reference: https://github.com/udacity/CarND-Object-Detection-Lab
+def Get_Predicted_Label(image, simulation=True):
+    if simulation:
+        graph_filename = 'model/sim_frozen_inference_graph.pb'
+    else:
+        graph_filename = 'model/carla_frozen_inference_graph.pb'
+    labels_filename = 'model/object-detection.pbtxt'
+    labels = load_labels(labels_filename)
+
+    labels_dic = {'green':1,
+                       'red':2,
+                       'yellow':3,
+                       'none':4}
+
+    print("Initializing TensorFlow...")
+    detection_graph = tf.Graph()
+    # configure for a GPU
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    # load trained tensorflow graph
+    with detection_graph.as_default():
+        graph_def = tf.GraphDef()
+            with tf.gfile.GFile(graph_filename, 'rb') as f:
+                graph_def.ParseFromString(f.read())
+                tf.import_graph_def(graph_def, name='')
+
+            sess = tf.Session(graph=detection_graph, config=config)
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+            # Each box represents a part of the image where a particular object was detected.
+            detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+            # Each score represent how level of confidence for each of the objects.
+            # Score is shown on the result image, together with the class label.
+            detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+            image_np = load_image_into_numpy_array(image)
+
+            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+            image_np_expanded = np.expand_dims(image_np, axis=0)
+
+            (boxes, scores, classes, num) = sess.run(
+              [detection_boxes, detection_scores, detection_classes, num_detections],
+                 feed_dict={image_tensor: image_np_expanded})
+
+            # Remove unnecessary dimensions
+            boxes   = np.squeeze(bindex = next((i for i, clsid in enumerate(class_) if clsid < 4), None)oxes)
+            class_  = np.int32(np.squeeze(classes).tolist())
+            scores  = np.squeeze(scores)
+
+            index = next((i for i, clsid in enumerate(class_) if clsid < 4), None)
+
+            return index
 
 
-class DeepLearningDetector:
-  def __init__(self):
-    PATH_TO_CKPT = 'models/sim_frozen_inference_graph.pb'
-    PATH_TO_LABELS = 'models/object-detection.pbtxt'
+
+
+def labels(labels_filename):
+    return [line.rstrip() for line in tf.gfile.GFile(labels_filename)]
+
+def load_image_into_numpy_array(image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -2,6 +2,19 @@ import rospy
 
 from styx_msgs.msg import TrafficLight
 from opencv_detector import recognize_traffic_lights
+from dl_detector import Get_Predicted_Label
+
+
+
+def traffic_light_msg_to_string_dl(traffic_light_msg):
+       if traffic_light_msg == 1:
+        return 'GREEN'
+    elif traffic_light_msg == 2:
+        return 'RED'
+    elif traffic_light_msg == 3:
+        return 'YELLOW'
+	 elif traffic_light_msg == 4:
+        return 'OFF'
 
 
 def traffic_light_msg_to_string(traffic_light_msg):
@@ -19,9 +32,11 @@ def traffic_light_msg_to_string(traffic_light_msg):
         return 'UNKNOWN'
 
 
+
 class TLClassifier(object):
     def __init__(self):
-        self.use_opencv = True
+        self.use_opencv = False
+		self.use_dl = True
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -38,4 +53,10 @@ class TLClassifier(object):
             rospy.logdebug("Found Traffic Light: %s", traffic_light_msg_to_string(traffic_light))
             return traffic_light
 
+		if self.use_dl:
+		    predicted_label = Get_Predicted_Label(image)
+
+			rospy.logdebug("Found Traffic Light: %s", traffic_light_msg_to_string_dl(predicted_label))
+
+			return predicted_label
         return TrafficLight.UNKNOWN
